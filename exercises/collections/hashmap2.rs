a747c0ce2c6fd146bdc86cddefdff4daedfaf82d
Christian Glassiognon <christian.glassiognon@techtonic.com>
2021-02-08 15:00:08 -0600

    Current Progress with rustlings

--- exercises/collections/hashmap2.rs
+++ exercises/collections/hashmap2.rs
@@ -12,7 +12,6 @@
 // Execute the command `rustlings hint collections4` if you need
 // hints.
 
-// I AM NOT DONE
 
 use std::collections::HashMap;
 
@@ -38,7 +37,14 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
         // TODO: Put new fruits if not already present. Note that you
         // are not allowed to put any type of fruit that's already
         // present!
-    }
+        match fruit {
+            Fruit::Pineapple => basket.entry(Fruit::Pineapple).or_insert(5),
+            Fruit::Banana => basket.entry(Fruit::Banana).or_insert(5),
+            Fruit::Apple => basket.entry(Fruit::Apple).or_insert(5),
+            Fruit::Lichi => basket.entry(Fruit::Lichi).or_insert(5),
+            Fruit::Mango => basket.entry(Fruit::Mango).or_insert(5)
+    };
+}
 }
 
 #[cfg(test)]
